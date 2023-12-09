d17e2f32314bd3738c0fd406df98eaa608ba1890
Guzelechkin <cot-oc-pb@mail.ru>
2023-12-09 17:59:00 +0500

    Initial commit

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,66 @@
+package ru.netology;
+
+public class Radio {
+    private int currentNumber;
+    private int currentVolume;
+    private int minNumber = 0;
+    private int maxNumber = 9;
+    private int minVolume = 0;
+    private int maxVolume = 10;
+
+    public int getCurrentNumber() {
+        return currentNumber;
+    }
+
+    public void setCurrentNumber(int newCurrentNumber) {
+        if (newCurrentNumber < minNumber) {
+            return;
+        }
+        if (newCurrentNumber > maxNumber) {
+            return;
+        }
+        currentNumber = newCurrentNumber;
+    }
+
+    public void nextNumber() {
+        if (currentNumber < maxNumber) {
+            currentNumber = currentNumber + 1;
+        } else {
+            currentNumber = minNumber;
+        }
+    }
+
+    public void prevNumber() {
+        if (currentNumber > minNumber) {
+            currentNumber = currentNumber - 1;
+        } else {
+            currentNumber = maxNumber;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < minVolume) {
+            newCurrentVolume = minVolume;
+        }
+        if (newCurrentVolume > maxVolume) {
+            newCurrentVolume = maxVolume;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+}
